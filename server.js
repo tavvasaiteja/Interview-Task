e36742eca645d9d7202d4f7f02ed8de52388c843
tavvasaiteja <saitejatavva@gmail.com>
2020-11-14 19:55:23 -0800

    updates to heroku

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static('./dist/task'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/task'));
 
-app.get('/*',function(req,res) {
-    res.sendFile('index.html',{root:'dist/task'});
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/task/index.html'));
 });
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
