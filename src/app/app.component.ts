779e7d18520ceece9e10c3ce3860f11e869e9be4
tavvasaiteja <saitejatavva@gmail.com>
2020-12-01 16:13:53 -0800

    changes made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,39 +8,54 @@ import { TaskService} from './task.service'
 export class AppComponent implements OnInit{
   title = 'task';
   spaceXData;
+  land_success;
+  selectedYear;
+  yearBtnActive = false;
+  launchBtnActive = false;
+  landBtnActive = false;
+  launch_success;
   Year=[{year:'2006'},{year:'2007'},{year:'2008'},{year:'2009'},{year:'2010'},{year:'2011'},{year:'2012'},
   {year:'2013'},{year:'2014'},{year:'2015'},{year:'2016'},{year:'2017'},{year:'2018'},
   {year:'2019'},{year:'2020'}];
-  constructor( private service: TaskService){}
+  constructor( private service: TaskService){
+    if(this.landBtnActive == true &&  this.yearBtnActive ==true && this.launchBtnActive == true){
+      this.service.getFilteredSpaceXData({year_launch:this.selectedYear,launch_success:this.launch_success,land_success:this.land_success}).subscribe(response=>{
+        this.spaceXData = response; 
+      });
+    
+    } 
+  }
+
   launchSuccess(success){
-    this.service.getLaunchSuccessData(success).subscribe((response)=>{
+    this.launch_success = success;
+    this.launchBtnActive = !this.launchBtnActive;
+    this.service.getFilteredSpaceXData({launch_success:this.launch_success}).subscribe((response)=>{
       this.spaceXData = response;
-      console.log("response for land_success filter",this.spaceXData);
-    },(err)=>{
-      console.log(err);
     })
   }
-  AllFilters(year){
-    this.service.getAllFiltersData(year).subscribe((response)=>{
+  yearFilter(year){
+    this.selectedYear= year;
+    this.yearBtnActive =  !this.yearBtnActive;
+    console.log('yearBtn',this.yearBtnActive);
+
+    this.service.getFilteredSpaceXData({launch_year:this.selectedYear}).subscribe((response)=>{
       this.spaceXData = response;
-      console.log("repsone for all the filtered data",this.spaceXData);
-    },(err)=>{
-      console.log(err);
     })
 
   }
   landSuccess(success){
-    this.service.getLaunchAndLandData(success).subscribe((response)=>{
+    this.land_success = success;
+    this.landBtnActive = !this.landBtnActive;
+    console.log('landBtn',this.landBtnActive);
+    this.service.getFilteredSpaceXData({land_success:this.land_success}).subscribe((response)=>{
       this.spaceXData = response;
-      console.log("response for land and launch data",this.spaceXData);
-    },(err)=>{
-      console.log(err);
     })
   }
-  ngOnInit(){
+   
+  ngOnInit(){   
+   
     this.service.getSpaceXData().subscribe((response)=>{
       this.spaceXData = response;
-      console.log("logging the spacexdata",this.spaceXData);
     },(err)=>{
       console.log(err);
     });
--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -6,26 +6,11 @@ import { HttpClient } from '@angular/common/http';
 })
 export class TaskService{
     baseUrl ='https://api.spaceXdata.com/v3/launches?limit=100';
-    launchSuccess;
-    landSuccess;
-    launchYear;
-    constructor(private http:HttpClient){}
-    
+    constructor(private http:HttpClient){}    
     getSpaceXData(){
         return this.http.get(this.baseUrl);
     }
-    getAllFiltersData(year){
-        this.launchYear = year;
-        return this.http.get(this.baseUrl+"&launch_year="+this.launchYear);
-
-    }
-    getLaunchSuccessData(launchSuccess){
-        this.launchSuccess = launchSuccess;
-        return this.http.get(this.baseUrl+'&launch_success='+this.launchSuccess);
-    }
-    getLaunchAndLandData(landSuccess){
-        this.landSuccess = landSuccess;
-        return this.http.get(this.baseUrl+'&launch_success='+this.launchSuccess+'&land_success='+this.landSuccess);
-
+    getFilteredSpaceXData(param:any){
+        return this.http.get(this.baseUrl,{params:param});
     }
 }
